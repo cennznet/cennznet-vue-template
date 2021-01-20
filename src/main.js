bb274711f418eb3d3bc437dfdf688eb9207fd8fb
Amy Wang <amy.wang@centrality.ai>
2021-01-21 12:05:58 +1300

    Add cennznet API to the project and sample code to just connet

--- src/main.js
+++ src/main.js
@@ -6,3 +6,24 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+const { Api } = require('@cennznet/api');
+
+async function main () {
+  // Initialise the provider to connect to the Azalea node
+  const provider = 'wss://cennznet.unfrastructure.io/public/ws';
+
+  // Create the API and wait until ready
+  const api = await Api.create({provider});
+
+  // Retrieve the chain & node information information via rpc calls
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version()
+  ]);
+
+  console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+}
+
+main().catch(console.error).finally(() => process.exit());
